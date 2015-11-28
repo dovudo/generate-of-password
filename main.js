a8e26b036e6bb65185eed7d678f4aebde3d5ed89
dovudo <dovudo98@gmail.com>
2015-11-28 15:26:52 +0200

    second fix

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-alert('Работает только в IE т.к в других браузерах нет поддержки изменений файла');
 var u, dir, way, beginNumber, endNumber;
+alert('Работает только в IE т.к в других браузерах нет поддержки изменений файла');
 u = prompt('Ведите некасаемое число (по типу 533...)','533');
 beginNumber = prompt('Введите с какого числа начать генерацию (не считая Н.Ч)');
 endNumber = prompt('Ведите на каком числе закончить (не считая Н.Ч)');
@@ -12,12 +12,12 @@ for(var i = beginNumber; i < endNumber; i++) { //генерируем чилса
 	}
 
 
-function cread_file(dir){ //создание файла в который будет записываться генерация
+function create_file(dir){ //создание файла в который будет записываться генерация
 	var fso, f1;
 	fso = new ActiveXObject("Scripting.FileSystemObject");
 	f1 = fso.CreateTextFile(way, true); //создание и название имени файла
 	f1.WriteLine(dir);	// запись в файл
-	alert("Идет генерация можете следить за ней с colsole");
+	
 	
 
 }
--- main2.js
+++ main2.js
@@ -1,7 +1,15 @@
-var way;
-way = prompt('Way to file', 'D://file.txt');
+var i, dir;
+var u = "533";
+for(var i = 11111; i < 33333; i++) { //генерируем чилса
+	dir = u + i;
+	console.log(dir);
+	}
 
 
-var fso, f1;
-fso = new ActiveXObject("Scripting.FileSystemObject");
-f1 = fso.CreateTextFile("testfile.txt", true);
+function cread_file(){ //создание файла в который будет записываться генерация
+	var fso, f1;
+	fso = new ActiveXObject("Scripting.FileSystemObject");
+	f1 = fso.CreateTextFile("D:\\533.txt", true); //создание и название имени файла
+	f1.WriteLine(dir);	// запись в файл
+	alert("Идет генерация можете следить за ней в colsole");
+	}
